58b27e1208f7b7540605e12471a806a635b39cf4
Enrico <77448275+bic-marz@users.noreply.github.com>
2025-12-14 00:39:46 +0000

    anyway i am talking to myself

---
+++ aoc/years/y2025/solutions/day11.py
@@ -0,0 +1,20 @@
+"""Day 11: Description TBD."""
+
+from __future__ import annotations
+
+from aoc.base import BaseChallenge, ChallengeIdentity
+
+
+class Day11(BaseChallenge):
+    identity = ChallengeIdentity(year=2025, day=11)
+
+    def solve_part1(self, data: list[str]) -> str:
+        # TODO: implement part 1
+        return "not implemented"
+
+    def solve_part2(self, data: list[str]) -> str:
+        # TODO: implement part 2
+        return "not implemented"
+
+
+Challenge = Day11
